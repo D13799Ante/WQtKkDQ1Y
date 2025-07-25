19a992d18c8ad7c9c315aa19ab9485f8db7f9a07
D13799Ante <thaws.skews_0k@icloud.com>
2025-07-25 10:09:56 +0100

    Change the file's logging configuration

--- 207NE/uryMqs/hQDBKe/blended-learning_wpWnCU_1747960446.py
+++ 207NE/uryMqs/hQDBKe/blended-learning_wpWnCU_1747960446.py
@@ -1,26 +1,46 @@
+以下是优化后的setuptools配置文件代码：
+
+```python
 from setuptools import setup, find_packages
- 
+
 classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Developers',
-  "Programming Language :: Python :: 3",
-  "Operating System :: Unix",
-  "Operating System :: MacOS :: MacOS X",
-  "Operating System :: Microsoft :: Windows",
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3',
+    'Operating System :: OS Independent',  # 改为OS Independent，表示支持所有操作系统
+    'License :: OSI Approved :: MIT License',
 ]
- 
+
 setup(
-  name='hillclimbers',
-  version='0.1.2',
-  description='A module to iteratively blend machine learning model predictions.',
-  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  url='',  
-  author='Matt-OP',
-  license='MIT', 
-  classifiers=classifiers,
-  keywords=['python', 'data', 'dataframe', 'machine learning', 'predictions', 'blending', 'pandas', 'numpy'], 
-  packages=find_packages(),
-  install_requires=['pandas', 'numpy', 'plotly', 'colorama'] 
+    name='hillclimbers',
+    version='0.1.2',
+    description='A module to iteratively blend machine learning model predictions.',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='https://github.com/Matt-OP/hillclimbers',  # 添加项目URL
+    author='Matt-OP',
+    author_email='your_email@example.com',  # 添加作者邮箱
+    license='MIT',
+    classifiers=classifiers,
+    keywords=['python', 'data', 'dataframe', 'machine learning', 'predictions', 'blending', 'pandas', 'numpy'],
+    packages=find_packages(),
+    install_requires=[
+        'pandas>=1.0.0',  # 指定pandas版本
+        'numpy>=1.18.0',  # 指定numpy版本
+        'plotly>=4.5.0',  # 指定plotly版本
+        'colorama>=0.4.3'  # 指定colorama版本
+    ],
+    python_requires='>=3.6',  # 指定Python版本要求
+    include_package_data=True,  # 包含package_data中的文件
+    zip_safe=False,  # 不建议打包为zip文件
 )
+```
+
+这段代码对原始代码进行了以下优化：
+1. 将操作系统支持改为OS Independent，表示支持所有操作系统。
+2. 添加了项目URL和作者邮箱。
+3. 指定了依赖库的版本要求。
+4. 添加了python_requires参数，指定Python版本要求。
+5. 添加了include_package_data参数，包含package_data中的文件。
+6. 添加了zip_safe参数，不建议打包为zip文件。
+
+以上优化可以提高项目的可维护性和兼容性。
